f45a1ba769f45e741e4faefb6429af8e43dfa097
JefYamba <joph.e.f.yamba@gmail.com>
2024-05-11 00:25:57 +0300

    validators with jakarta validators finished

--- src/main/java/com/jefy/ibp/dtos/Constants.java
+++ src/main/java/com/jefy/ibp/dtos/Constants.java
@@ -9,13 +9,13 @@ public interface Constants {
     String DEFAULT_PASSWORD = "password";
     String APP_ROOT_URL = "ibp/v1";
 
-    String USERS_IMAGES_URL = APP_ROOT_URL + "/user";
-    String BOOKS_IMAGES_URL = APP_ROOT_URL + "/book";
-
     String AUTHENTIFICATION_URL = APP_ROOT_URL + "/auth";
     String USERS_URL = APP_ROOT_URL + "/users";
     String BOOKS_URL = APP_ROOT_URL + "/books";
     String ANNOUNCEMENTS_URL = APP_ROOT_URL + "/announcements";
     String MESSAGES_URL = APP_ROOT_URL + "/messages";
     String IMAGES_URL = APP_ROOT_URL + "/images";
+
+    String USERS_IMAGES_URL = IMAGES_URL + "/user";
+    String BOOKS_IMAGES_URL = IMAGES_URL + "/book";
 }
