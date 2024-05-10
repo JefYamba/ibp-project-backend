18d61d3d6611c23d5404745af89634ec432e9c17
JefYamba <joph.e.f.yamba@gmail.com>
2024-05-10 04:55:13 +0300

    global exception handler

--- src/main/java/com/jefy/ibp/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/jefy/ibp/exceptions/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.LocalDateTime;
 
